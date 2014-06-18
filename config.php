d2bddb8b1519f6d2c68fcc0ad8a8696bd193bd83
Niels de Blaauw <niels.de.blaauw@gmail.com>
2014-06-18 22:44:59 +0200

    Original script imported from http://infinite.ibasics.biz/index.php#download

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php 
+# ------------------------------------------------
+# License and copyright:
+# See license.txt for license information.
+# ------------------------------------------------
+
+# Database information goes here. Server, user, password and database.
+$MySQL_server   = '';
+$MySQL_user     = '';
+$MySQL_password = '';
+$MySQL_database = '';
+
+# ------------------------------------------------
+
+# Get the rest of the config auto-magically
+include("get_config_vars.php");
+?>
